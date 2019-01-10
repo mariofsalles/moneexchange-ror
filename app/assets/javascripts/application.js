73a607f402e62d36d8e099b0668ecf8a5bb95d3b
Mario Salles <mariofsalles@gmail.com>
2019-01-10 21:51:50 +0000

    Include bootstrap and jquery, using yarn

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap/dist/js/bootstrap
+//= require jquery
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
